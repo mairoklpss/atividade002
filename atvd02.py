11fbf669138b661614bafd0c53c5ae5f3c979fcf
mairoklpss <mairalopesma@gmail.com>
2024-09-04 15:00:51 -0300

    conversao de °C para °F

---
+++ atvd02.py
@@ -0,0 +1,5 @@
+# Crie um programa que converta uma temperatura dada em Celsius para Fahrenheit usando a fórmula: F = C * 9/5 + 32.
+temp = float(input("quanto graus celsius está fazendo? "))
+f = temp * 9/5 + 32
+
+print(f"a temperatura atual é {temp}. A conversão para Fahrenheit é {f}.")
